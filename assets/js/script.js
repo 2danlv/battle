a77cb105293dae51378df5e3bbb396e5fd0b8965
2danlv <danbtrs@yahoo.com.vn>
2023-01-11 22:07:45 +0700

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ jQuery(document).ready(function($) {
         brandSlider.owlCarousel({
           center: true,
           loop: true,
+          autoplay:true,
           items: 3,
            smartSpeed: 500,
            nav: true,
@@ -25,10 +26,29 @@ jQuery(document).ready(function($) {
         brandSlider.owlCarousel({
           loop: true,
           items: 6,
+          autoplay:true,
           smartSpeed: 300,
           nav: false,
           dot: true,
           mouseDrag: false,
           touchDrag: false,
-      });
+          responsive : {
+    // breakpoint from 0 up
+    0 : {
+      items: 2,
+      nav: true,
+      dots: false
+    },
+    // breakpoint from 480 up
+    480 : {
+      items: 3,
+      nav: true,
+      dots: false
+    },
+    // breakpoint from 768 up
+    768 : {
+      items: 6
+    }
+  }
+});
 });
